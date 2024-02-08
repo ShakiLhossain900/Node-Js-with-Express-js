ac2201dbd3e9a6e172d00b7ea12b6f4e527cff83
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-08 19:50:51 +0600

    adding

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express(); // running express as a funciton
 app.use((req, res, next) => {
     console.log(' In the middlware');
     next(); //allow kore next middlware function y jaite
-})
+});
 
 app.use((req, res, next) => {
     console.log(' In another middlware');
-})
+
+});
 
 const server = http.createServer(app); // app pass koreci to create server
 
