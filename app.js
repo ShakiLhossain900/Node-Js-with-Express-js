88ac4d90d6b2ee9d61991910079e7b4ade025f70
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-08 19:29:01 +0600

    adding

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const express = require('express');
 
+const app = express(); // running express as a funciton 
+
 const server = http.createServer();
 
 server.listen(3000);
