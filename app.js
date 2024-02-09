505368aafc0a9fad93764f15bb1e8eb7e24afc1d
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-09 18:20:48 +0600

    adding

--- app.js
+++ app.js
@@ -11,12 +11,23 @@ const app = express(); // running express as a funciton
 //     // calling next function middlware to middlware jowa jay
 // });
 
+//always run middlware
+app.use('/',(req, res, next) => {
+    console.log('this always run');
+    next();
+})
+
 //middlware
 app.use('/add-product',(req, res, next) => {
     console.log(' In another middlware');
-    res.send('<h1>add "product page" </h1>') //send response //html content send 
+    res.send('<h1>add "product page" </h1>'); 
+    res.send('<form action="/procut" method="POST"><input type="text"><button type="submit>Add product</button></form>')
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 //middlware
 app.use('/',(req, res, next) => {
